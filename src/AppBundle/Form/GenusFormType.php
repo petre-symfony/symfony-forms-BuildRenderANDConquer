d03bd95addb8efa507d463a009eee1c874b12f30
petrero <petrero@gmail.com>
2017-03-13 21:20:27 +0200

    Copy code from previous branch

---
+++ src/AppBundle/Form/GenusFormType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use AppBundle\Entity\SubFamily;
+use AppBundle\Repository\SubFamilyRepository;
+
+class GenusFormType extends AbstractType{
+  public function buildForm(FormBuilderInterface $builder, array $options) {
+    $builder->add('name')
+      ->add('speciesCount')
+      ->add('subFamily', EntityType::class, [
+        'placeholder'   => 'Choose a Sub Family',
+        'class'         => subFamily::class,
+        'query_builder' => function(SubFamilyRepository $repo){
+          return $repo->createAlphabeticalQueryBuilder(); 
+        }  
+      ])      
+      ->add('funFact')
+      ->add('isPublished', ChoiceType::class, [
+        'choices' => [
+          'Yes' => true,
+          'No'  => false  
+        ]    
+      ])
+      ->add('firstDiscoveredAt');      
+  }
+  
+  public function configureOptions(OptionsResolver $resolver) {
+    $resolver->setDefaults([
+       'data_class' => 'AppBundle\Entity\Genus' 
+    ]);
+  }
+}
