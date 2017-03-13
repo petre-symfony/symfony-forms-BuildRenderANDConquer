3029a9511407a2eb6b43d8668c30a2f65584c537
petrero <petrero@gmail.com>
2017-03-13 17:48:19 +0200

    Copy code from previous branch

---
+++ src/AppBundle/Form/GenusFormType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GenusFormType extends AbstractType{
+  public function buildForm(FormBuilderInterface $builder, array $options) {
+    $builder->add('name')
+      ->add('speciesCount')
+      ->add('funFact');      
+  }
+}
