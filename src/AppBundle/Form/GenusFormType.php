0f1a8fa75086fb2789694f915af4a0f75e7d5a81
petrero <petrero@gmail.com>
2017-03-13 17:42:14 +0200

    Here's how it works: in buildForm() start adding fields {dollar_sign}builder->add() and then name to create a 'name' field. Keep going, add('speciesCount') and add('funFact'). Right now, those field names can be anything - you'll see why in a second.
    The form is built after writing about 4 lines of code! Let's go render it!

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -8,6 +8,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class GenusFormType extends AbstractType{
   public function buildForm(FormBuilderInterface $builder, array $options) {
-    
+    $builder->add('name')
+      ->add('speciesCount')
+      ->add('funFact');      
   }
 }
