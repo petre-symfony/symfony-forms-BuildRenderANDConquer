b5322b5c7c9705bc7413272437d69bef19e05ca7
petrero <petrero@gmail.com>
2017-03-14 18:49:06 +0200

    When you get to the form, it's completely blank. How could we add default data to the form?
    Well, it turns out answering that question is exactly the same as answering the question
    
    How do we create an edit form?
    Let's tackle that.
    
    In GenusAdminController, I'm going to be lazy: copy the entire newAction() and update the URL to /genus/{id}/edit. Give it a different route name: admin_genus_edit and call it editAction().
    
    Our first job should be to query for a Genus object. I'll be lazy again and just type-hint an argument with Genus.
    
    Thanks to the param converter from SensioFrameworkExtraBundle, this will automatically query for Genus by using the {id} value.

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller\Admin;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Genus;
 use AppBundle\Form\GenusFormType;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -30,6 +31,32 @@ class GenusAdminController extends Controller
      * @Route("/genus/new", name="admin_genus_new")
      */
     public function newAction(Request $request)
+    {
+        $form = $this->createForm(GenusFormType::class);
+        
+        //only handles data on POST
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+          $genus = $form->getData();
+          
+          $em = $this->getDoctrine()->getManager();
+          $em->persist($genus);
+          $em->flush();
+          
+          $this->addFlash('success', 'Genus created');
+          
+          return $this->redirectToRoute('admin_genus_list');
+        }
+        
+        return $this->render('admin/genus/new.html.twig', [
+            'genusForm' => $form->createView()
+        ]);
+    }
+    
+    /**
+     * @Route("/genus/{id}/edit", name="admin_genus_edit")
+     */
+    public function editAction(Request $request, Genus $genus)
     {
         $form = $this->createForm(GenusFormType::class);
         
